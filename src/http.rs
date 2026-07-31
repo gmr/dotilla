f3ef82ef379207802d18264f6e5534f9741d247d
Gavin M. Roy <gavinmroy@gmail.com>
2026-07-31 18:47:34 -0400

    Add SocketAddr import and refactor bind address
    Replaced string formatting with SocketAddr to use a more idiomatic
    approach for constructing socket addresses in Rust.

--- src/http.rs
+++ src/http.rs
@@ -1,5 +1,6 @@
 use crate::config::Config;
 use axum::{Router, http::StatusCode, response::IntoResponse, routing::get};
+use std::net::SocketAddr;
 
 async fn handler_404() -> impl IntoResponse {
     (StatusCode::NOT_FOUND, "nothing to see here")
@@ -13,9 +14,7 @@ pub async fn serve(config: &Config) {
         )
         .fallback(handler_404);
 
-    let listener =
-        tokio::net::TcpListener::bind(format!("{}:{}", config.listen_address, config.port))
-            .await
-            .unwrap();
+    let addr = SocketAddr::new(config.listen_address, config.port);
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, router).await.unwrap();
 }
